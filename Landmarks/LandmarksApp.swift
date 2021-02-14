63a1d3ddc7433e6d11a19147a2ca6124bf420894
magcipie2007 <magicpie2007@gmail.com>
2021-02-15 00:56:36 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hiroyuki shoji on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
