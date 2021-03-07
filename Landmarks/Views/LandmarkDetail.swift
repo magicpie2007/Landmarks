6d8e7a4808e751b8e83ef11011e6df3646c4ea9d
magcipie2007 <magicpie2007@gmail.com>
2021-03-07 19:00:04 +0900

    Change for the 2nd tutorial of Landmarks
    I complete the 2nd tutorial "Building List and Navigation" for SwiftUI study and commit all changes.

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by hiroyuki shoji on 2021/03/07.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate).ignoresSafeArea( edges: .top).frame(height: 300)
+            
+            CircleImage(image: landmark.image).offset(y: -130).padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state).font(.subheadline)
+                }.font(.subheadline).foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)").font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
